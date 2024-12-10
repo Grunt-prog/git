ae62341e8e784ee547542ea8fb337df199a48a82
Grunt-prog <ritesh7153@gmail.com>
2024-12-11 00:44:39 +0530

    updated code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Dockerized Flask!"
+    return "Hello, Dockerized Flask! hi hello"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003)
